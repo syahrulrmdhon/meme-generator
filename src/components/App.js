cde52cd1c2641ddf506a16c308ab4ef0b45c6a00
Syahrul Romadhon <syahrulrmdhon2@gmail.com>
2018-09-24 00:53:34 +0700

    add app component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Selamat Datang di Meme Generator</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
