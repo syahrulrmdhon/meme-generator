627ad91f972af1cdba41793a2ccc5e79ef02b938
Syahrul Romadhon <syahrulrmdhon2@gmail.com>
2018-09-24 02:32:12 +0700

    set reducers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import thunk from 'redux-thunk';
+import { fetchMemes } from './actions';
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+const store = createStore(rootReducer,applyMiddleware(thunk));
+store.subscribe(() => console.log('store', store.getState()));
+store.dispatch(fetchMemes());
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+,document.getElementById('root'));
