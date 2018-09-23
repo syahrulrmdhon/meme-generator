38c16df70be675695688f604e34735e847a1b37e
Syahrul Romadhon <syahrulrmdhon2@gmail.com>
2018-09-24 05:44:36 +0700

    listing memes name and some styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import '../styles/style.css';
 
 class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            memeLimit: 15
+        }
+    }
+    onclicks() {
+        this.setState({
+            memeLimit: this.state.memeLimit + 5
+        })
+    }
     render() {
         return (
-            <div>
-                <h1>Selamat Datang di Meme Generator</h1>
+            <div className="container h-100">
+                <div className="row h-100 justify-content-center align-items-center">
+                    <h1>Selamat Datang di Meme Generator</h1>
+                </div>
+                <div style={{textAlign: 'center'}}>
+                {
+                    this.props.memes.slice(0,this.state.memeLimit).map((meme, index) => {
+                        return (
+                            <h4 key={index}>{meme.name}</h4>
+                        )
+                    })
+                }
+                    <button className='form-control btn-info' onClick={() => this.onclicks()}>Load More....</button>
+                </div>
             </div>
         );
     }
 }
 
-export default App;
+function mapStateToProps( state ) {
+    return state;
+}
+
+export default connect(mapStateToProps, null)(App);
