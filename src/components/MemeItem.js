199fecc120f6a9ebe60690f6aaa6a7ee13f7ac9a
Syahrul Romadhon <syahrulrmdhon2@gmail.com>
2018-09-25 00:56:07 +0700

    styling meme item

--- src/components/MemeItem.js
+++ src/components/MemeItem.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
 class MemeItem extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            hovered: false
+        }
+    }
     render() {
         return (
-            <div className="meme-item">
-                <img className="meme-img" src={this.props.meme.url} alt={this.props.meme.name}/>
-                <h5 className="meme-text">{this.props.meme.name}</h5>
+            <div 
+                className="meme-item"
+                onMouseEnter={() => this.setState({hovered:true})}
+                onMouseLeave={() => this.setState({hovered:false})}
+            >
+                <img className={this.state.hovered ? "meme-img dark-img" : "meme-img" } src={this.props.meme.url} alt={this.props.meme.name}/>
+                <h5 className={this.state.hovered ? "meme-text" : "no-text"}>{this.props.meme.name}</h5>
             </div>
         );
     }
