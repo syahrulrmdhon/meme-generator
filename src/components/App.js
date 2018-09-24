a420c5d5401ed172001d6c384d560fbe525295c3
Syahrul Romadhon <syahrulrmdhon2@gmail.com>
2018-09-25 01:41:22 +0700

    styling form

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import '../styles/style.css';
 import MemeItem from './MemeItem';
+import { FormControl } from 'react-bootstrap';
 
 class App extends Component {
     constructor() {
@@ -20,7 +21,22 @@ class App extends Component {
         return (
             <div className="container h-100">
                 <div className="row h-100 justify-content-center align-items-center">
-                    <h1>Bikin Memes</h1>
+                    <h1>Bikin Memes</h1><br/>
+                </div>
+                <div  style={{marginTop: '50px'}}>
+                    <h4 style={{textAlign: 'center'}}><i>Tulis kata-katanya</i></h4>
+                    <form className="col-lg-6 offset-lg-3 ">
+                        <div className="row justify-content-center"  style={{marginTop: '10px'}}>
+                            <FormControl 
+                                className="form-control" 
+                                type="text" placeholder="Teks Atas">
+                            </FormControl>
+                            <FormControl 
+                                className="form-control" 
+                                type="text" placeholder="Teks Bawah">
+                            </FormControl>
+                        </div>
+                    </form>
                 </div>
                 <div className="row h-100 justify-content-center align-items-center">
                 {
